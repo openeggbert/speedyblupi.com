994a9dda8f4b3b81f044b1a7edebca91fd152c35
Robert Vokac <robertvokac@robertvokac.com>
2026-05-17 01:44:32 +0200

    Experimental version of Speedy Blupi 2013 were added - 60 FPS, 120 FPS and 144 FPS

--- SpeedyBlupi201360FPS/WindowsPhoneSpeedyBlupi.js
+++ SpeedyBlupi201360FPS/WindowsPhoneSpeedyBlupi.js
@@ -71,7 +71,7 @@ var ENVIRONMENT_IS_SHELL = !ENVIRONMENT_IS_WEB && !ENVIRONMENT_IS_NODE && !ENVIR
 
 // --pre-jses are emitted after the Module integration code, so that they can
 // refer to Module (if they choose; they can also define Module)
-// include: /tmp/tmpamgma08s.js
+// include: /tmp/tmpcvaq9y35.js
 
   if (!Module['expectedDataFileDownloads']) Module['expectedDataFileDownloads'] = 0;
   Module['expectedDataFileDownloads']++;
@@ -208,14 +208,14 @@ Module['FS_createPath']("/", "worlds", true, true);
 
   })();
 
-// end include: /tmp/tmpamgma08s.js
-// include: /tmp/tmp9u8yzdvz.js
+// end include: /tmp/tmpcvaq9y35.js
+// include: /tmp/tmpz0wzd91x.js
 
     // All the pre-js content up to here must remain later on, we need to run
     // it.
     if ((typeof ENVIRONMENT_IS_WASM_WORKER != 'undefined' && ENVIRONMENT_IS_WASM_WORKER) || (typeof ENVIRONMENT_IS_PTHREAD != 'undefined' && ENVIRONMENT_IS_PTHREAD) || (typeof ENVIRONMENT_IS_AUDIO_WORKLET != 'undefined' && ENVIRONMENT_IS_AUDIO_WORKLET)) Module['preRun'] = [];
     var necessaryPreJSTasks = Module['preRun'].slice();
-  // end include: /tmp/tmp9u8yzdvz.js
+  // end include: /tmp/tmpz0wzd91x.js
 // include: /rv/data/development/github.com/openeggbert/mobile-eggbert/cmake/web/pre.js
 // pre.js - executed before the Emscripten Module is initialized.
 // Mounts IDBFS at /save so that IsolatedStorage writes (SpeedyBlupi save file)
@@ -258,13 +258,13 @@ window.addEventListener('beforeunload', function () {
     }
 });
 // end include: /rv/data/development/github.com/openeggbert/mobile-eggbert/cmake/web/pre.js
-// include: /tmp/tmp78zpt94w.js
+// include: /tmp/tmp61lcfe58.js
 
     if (!Module['preRun']) throw 'Module.preRun should exist because file support used it; did a pre-js delete it?';
     necessaryPreJSTasks.forEach((task) => {
       if (Module['preRun'].indexOf(task) < 0) throw 'All preRun tasks that exist before user pre-js code should remain after; did you replace Module or modify Module.preRun?';
     });
-  // end include: /tmp/tmp78zpt94w.js
+  // end include: /tmp/tmp61lcfe58.js
 
 
 var arguments_ = [];
